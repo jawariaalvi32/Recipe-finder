b474c30b284c5269c854a81e010bc547fbe960cb
Jawaria Alvi <jawariaalvi32@gmail.com>
2020-11-17 06:59:33 +0500

    Add some styling

--- app.js
+++ app.js
@@ -5,17 +5,16 @@ let imgr = document.getElementById('single-meal-img');
 let searchDiv = document.getElementById('search');
 
 let searchRecipe = () => {
-    clearDivs();
-
-    let searchIp = document.getElementById('search-ip').value;
-    let searchApi = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchIp}`;
+  clearDivs()
+  let searchIp = document.getElementById('search-ip').value;
+  let searchApi = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchIp}`;
 
     fetch(searchApi).then(api => api.json())
     .then((data) => {
         console.log(data)
         for (let index = 0; index < data.meals.length; index++) {
             let box = document.createElement('div');
-            box.setAttribute('class','col-md-3 col-sm-4 col-xs-12 m-91 text-center');
+            box.setAttribute('class','col-lg-3 col-md-4 col-sm-6 col-xs-12 m-91 text-center');
             box.setAttribute('id',data.meals[index].idMeal);
             let img = document.createElement('img');
             img.setAttribute('src', data.meals[index].strMealThumb)
@@ -56,26 +55,27 @@ let singleMeal = (data) => {
   // Meal Heading
   let h1 = document.createElement('h1');
   let h1Text = document.createTextNode(data.meals[0].strMeal)
+  h1.setAttribute('class','mt-5 mb-4')
   h1.appendChild(h1Text);
   imgr.appendChild(h1)
 
   // Meal Image
   let box = document.createElement('div');
-  box.setAttribute('class','col-md-3');
   box.setAttribute('id',data.meals[0].idMeal);
   let img = document.createElement('img');
   img.setAttribute('src', data.meals[0].strMealThumb)
-  img.setAttribute('class','meal');
+  img.setAttribute('class','meal meal-w');
   box.appendChild(img);
   imgr.appendChild(box);
 
-  // Meal Tags
+  // Meal Category
   let tags = (data.meals[0].strTags == null) ? 0 : data.meals[0].strTags.split(',');
   for (let index = 0; index < tags.length - 1; index++) {
-    let p = document.createElement('p');
-    let pText = document.createTextNode(tags[index])
-    p.appendChild(pText);
-    mealCategory.appendChild(p)
+    let spCategory = document.createElement('span');
+    spCategory.setAttribute('class', 'badge badge-secondary m-2')
+    let spCategoryTxt = document.createTextNode(tags[index])
+    spCategory.appendChild(spCategoryTxt);
+    mealCategory.appendChild(spCategory)
   }
 
   // Meal Instructions
